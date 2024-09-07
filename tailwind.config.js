24e3f7d21f5d354aff36047fc04f24bdeca13fcc
isaiahpius <isaiahpius73@gmail.com>
2024-09-07 15:08:18 +0100

    mobile responsive

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./**/*.html"],
-  theme: {
+  theme: { 
+    fontFamily: {
+      sans: ['"Montserrat"', "sans-serif"],
+    },
     extend: {},
   },
   plugins: [],
